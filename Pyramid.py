29f2094bd09095c253ec396cea95fb5f1ac00a39
Ishan Nalawade <125126646+ishaan2304@users.noreply.github.com>
2025-01-29 22:18:29 +0530

    Create Pyramid.py

---
+++ Pyramid.py
@@ -0,0 +1,7 @@
+def pyramid(rows):
+    for i in range(1, rows + 1):
+        print(("*" * (2 * i - 1)).center(2 * rows - 1))
+
+# Example: Print a pyramid with 5 rows
+rows = int(input("Enter the number of rows: "))
+pyramid(rows)
